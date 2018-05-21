2b5ccd2e154b330c94f24e6ad8704c90814751a0
SunilSubedi <sunilsubedi9@gmail.com>
2018-05-21 10:27:20 +0545

    Arjune don

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Arjune Kauwaa
                 </div>
 
                 <div class="links">
